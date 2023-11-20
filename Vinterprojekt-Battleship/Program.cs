022c99ed04f0f4a7e2d0241b5cddd03bbba665da
Ishaan Vepakomma <ishaan.vepakomma@elev.ga.ntig.se>
2023-11-20 08:40:09 +0100

    first commit

---
+++ Vinterprojekt-Battleship/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security.Permissions;
+using Raylib_cs;
+
+Raylib.InitWindow(768,1024, "Battelship");
+Raylib.SetTargetFPS(60);
+
